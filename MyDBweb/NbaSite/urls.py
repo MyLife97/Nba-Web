81ee32006ac9dc12b57c548e8246f9ae45c9051e
MyLife97 <bowenyang@buaa.edu.cn>
2018-10-10 19:52:33 +0800

    Connection Success.

--- MyDBweb/NbaSite/urls.py
+++ MyDBweb/NbaSite/urls.py
@@ -6,4 +6,6 @@ app_name = 'NbaSite'
 
 urlpatterns = [
     path('', views.home, name='home'),	
+    path('player=<str:player>/', views.search_by_name, name = 'search_by_name')
+    # path('ajax_test', views.ajax_test, name = 'ajax_test'),
 ]
--- MyDBweb/NbaSite/views.py
+++ MyDBweb/NbaSite/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from .models import Player, Team
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
+
+import json
 # Create your views here.
 
-def home(request):
 
+
+def home(request):
 	return render(request, 'NbaSite/home.html')
+
+def search_by_name(request, player):
+	try:
+		 = Question.objects.get(pk = question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
+
+
+	return HttpResponse(player)
+
+# def ajax_test(request):
+# 	name = 'James'
+# 	return JsonResponse(name, safe = False)	
+
+# def ajax_test(request):
+#     user_name = request.POST.get("username")
+#     password = request.POST.get("password")
+#     print(user_name, password)
+#     # i += 1
+#     return HttpResponse("i")
