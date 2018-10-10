94d9a2b08a7b7e7e2f2206bb487105ae61e2bff9
MyLife97 <bowenyang@buaa.edu.cn>
2018-10-11 00:01:04 +0800

    All Functions are correct.

--- MyDBweb/NbaSite/urls.py
+++ MyDBweb/NbaSite/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     #Search Pattern
     path('search_player=<str:player_name>/', views.search_by_name, name = 'search_by_name'),
     #Add Pattern
-    path('add_player=<str:player_name>_team=<str:team>/', views.add_player, name = 'add_player'),
+    path('add_player=<str:player_name>_team=<str:team_name>/', views.add_player, name = 'add_player'),
     #Delete Pattern
     path('delete_player=<str:player_name>/', views.delete_by_name, name = 'delete_by_name'),
     #Change Pattern
-    path('change_player=<str:player_name>_team=<str:team>/', views.change_team_by_name, name = 'change_team_by_name'),
+    path('change_player=<str:player_name>_team=<str:team_name>/', views.change_team_by_name, name = 'change_team_by_name'),
 
 ]
--- MyDBweb/NbaSite/views.py
+++ MyDBweb/NbaSite/views.py
@@ -16,44 +16,58 @@ import datetime
 def home(request):
 	return render(request, 'NbaSite/home.html')
 
-
-#
 def search_by_name(request, player_name):
 	try:
 		player = Player.objects.get(player_name = player_name)
-		print(player_name)
 		# content = request.POST.get('name')
 		return HttpResponse("Team:" + str(player.team) + " Begin Date:" + str(player.begin_date))
 	except Player.DoesNotExist:
-		print("not found")
-		return HttpResponse("False")
+		return HttpResponse("Player %s is Not in the Table!" % player_name)
 	# return HttpResponse(player)
 
 def delete_by_name(request, player_name):
 	try:
 		old_player = Player.objects.get(player_name = player_name)
 		old_player.delete()
-		return HttpResponse("Delete Succeed.")
+		# return HttpResponse("Delete Succeed.")
+		return HttpResponse("Player %s has been deleted successfully." % player_name)
 	except Player.DoesNotExist:
 		return HttpResponse("Player %s is Not in the Table!" % player_name)
 
-def change_team_by_name(request, player_name, new_team):
+def change_team_by_name(request, player_name, team_name):
 	try:
 		player = Player.objects.get(player_name = player_name)
+		try:
+			new_team = Team.objects.get(team_name = team_name)
+		except Team.DoesNotExist:
+			new_team = Team(team_name = team_name)
+			new_team.save()
+
 		player.team = new_team
 		player.save()
 		return HttpResponse('Change %s\'s team succeed.' % player_name)
-	except Palyer.DoesNotExist:
+	except Player.DoesNotExist:
 		return HttpResponse("Change Fail. Player %s is Not in the Table!" % player_name)
 
-def add_player(request, player_name, begin_date = datetime.datetime.now(), team = ''):
+def add_player(request, player_name, begin_date = datetime.datetime.now(), team_name = ''):
+
 	try:
 		old_player = Player.objects.get(player_name = player_name)
-		return HttpResponse("False")
+		return HttpResponse("Player %s is already in the Table." %player_name)
 	except Player.DoesNotExist:
-		new_player = Player(player_name = player_name, begin_date = begin_date, team = team)
-		new_player.save()
-		return HttpResponse("True")
+		try:
+			team = Team.objects.get(team_name = team_name)
+		except Team.DoesNotExist:
+			team = Team(team_name = team_name)
+			team.save()
+			new_player = Player(player_name = player_name, begin_date = begin_date, team = team)
+			new_player.save()
+			return HttpResponse("Player %s has been added into %s." %(player_name, team_name))
+			# return HttpResponse("TeamNotFound")
+
+	new_player = Player(player_name = player_name, begin_date = begin_date, team = team)
+	new_player.save()
+	return HttpResponse("Player %s has been added into %s." %(player_name, team_name))
 	#return render(request, 'polls/detail.html', {'question': question})
 
 	# return HttpResponse(player)
