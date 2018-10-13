011d7a9d337d2312befd6495b65d9ae7ad74bd14
liuliang <1>
2018-10-13 10:13:52 +0800

    a

--- MyDBweb/NbaSite/models.py
+++ MyDBweb/NbaSite/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Team(models.Model):
-	
-	team_name = models.CharField(max_length = 200)
-	team_city = models.CharField(max_length = 200)
-	
-	def __str__(self):
-		return self.team_name
+    team_name = models.CharField(max_length=200)
+    team_city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.team_name
+
 
 class Player(models.Model):
-	player_name = models.CharField(max_length = 200)
-	begin_date = models.DateField('date attend')
-	
-	team = models.ForeignKey(Team, on_delete = models.CASCADE)
+    player_name = models.CharField(max_length=200)
+    begin_date = models.DateField('date attend')
 
-	def __str__(self):
-		return self.player_name
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
-	# def was_published_recently(self):
-		# return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
-		
+    def __str__(self):
+        return self.player_name
+
+# def was_published_recently(self):
+# return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
