4380b88c2abb58c666a2e7d07131ac9b4dab18fa
liuliang <1>
2018-10-13 12:46:19 +0800

    nothing

--- MyDBweb/NbaSite/models.py
+++ MyDBweb/NbaSite/models.py
@@ -4,21 +4,6 @@ from django.db import models
 # Create your models here.
 
 class Team(models.Model):
-<<<<<<< HEAD
-    team_name = models.CharField(max_length=200)
-    team_city = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.team_name
-
-
-class Player(models.Model):
-    player_name = models.CharField(max_length=200)
-    begin_date = models.DateField('date attend')
-
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-=======
-	
 	team_name = models.CharField(primary_key = True, max_length = 200)
 	team_city = models.CharField(max_length = 200)
 	
@@ -26,13 +11,12 @@ class Player(models.Model):
 		return self.team_name
 
 class Player(models.Model):
-	player_name = models.CharField(primary_key = True, max_length = 200)
-	begin_date = models.DateField('date been selected')
-	
-	team = models.ForeignKey(Team, on_delete = models.CASCADE)
->>>>>>> 8afd2e11426d3fdd3ba30c7d013a8f49e3681916
+    player_name = models.CharField(primary_key = True, max_length = 200)
+    begin_date = models.DateField('date been selected')
 
-    def __str__(self):
+    team = models.ForeignKey(Team, on_delete = models.CASCADE)
+
+    def __str__(self): 
         return self.player_name
 
 # def was_published_recently(self):
